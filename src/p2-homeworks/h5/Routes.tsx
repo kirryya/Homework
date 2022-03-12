c595c62341fe240b2a53bb71c58068a158b84065
kirryya <kir1987@inbox.ru>
2022-03-12 12:01:58 +0300

    deploy

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Navigate, Route, Routes as Switch} from "react-router-dom";
+import {Route, Routes as Switch} from "react-router-dom";
 import PreJunior from "./pages/PreJunior";
 import Error404 from "./pages/Error404";
 import Junior from "./pages/Junior";
@@ -10,7 +10,7 @@ function Routes() {
     return (
         <div>
             <Switch>
-                <Route path={'/'} element={<Main/>}/>
+                <Route path={'/Homework'} element={<Main/>}/>
                 <Route path={'/pre-junior'} element={<PreJunior/>}/>
                 <Route path={'/junior'} element={<Junior/>}/>
                 <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
