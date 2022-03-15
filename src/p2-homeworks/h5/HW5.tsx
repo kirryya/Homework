0942f1bcc27a17ad7cd5e2a6ce07154ac3fc3c94
kirryya <kir1987@inbox.ru>
2022-03-15 12:13:38 +0300

    deploy

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -6,6 +6,7 @@ import {HashRouter} from "react-router-dom";
 function HW5() {
     return (
         <div>
+            <div style={{fontSize: "x-large"}}>React Homeworks</div>
             <HashRouter>
                 <Header/>
                 <Pages/>
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import s from ".././h3/Greeting.module.css"
+import b from "./Header.module.css"
 
 
 function Header() {
     return (
-        <div>
+        <div className={b.header}>
             <NavLink to={'/pre-junior'} className={s.button}> PRE_JUNIOR </NavLink>
             <NavLink to={'/junior'} className={s.buttons}> JUNIOR </NavLink>
             <NavLink to={'/junior-plus'} className={s.button}> JUNIOR_PLUS </NavLink>
