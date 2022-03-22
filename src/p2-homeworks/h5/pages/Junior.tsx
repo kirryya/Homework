df4654757d31df9e0ba3dc3e62abfeca03ae03e6
kirryya <kir1987@inbox.ru>
2022-03-22 17:43:06 +0300

    hw7

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import HW7 from "../../h7/HW7";
 
 const Junior = () => {
     return (
         <div>
-            {/*<HW7/>
-            <HW8/>
+            <HW7/>
+            {/*<HW8/>
             <HW9/>*/}
         </div>
     );
--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import s from "./common/c5-SuperSelect/SuperSelect.module.css"
 
 const arr = ['x', 'y', 'z']
 
 function HW7() {
-    const [value, onChangeOption] = useState(arr[1])
+    const [value, onChangeOption] = useState(arr[0])
 
     return (
         <div>
@@ -13,7 +14,7 @@ function HW7() {
             homeworks 7
 
             {/*should work (должно работать)*/}
-            <div>
+            <div >
                 <SuperSelect
                     options={arr}
                     value={value}
